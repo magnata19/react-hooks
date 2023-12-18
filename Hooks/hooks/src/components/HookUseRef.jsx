cbaa95a412ed43763862e8d46b969bef19484f17
Davidson <dehpacifico@gmail.com>
2023-12-18 20:49:32 -0300

    estudando useRef

--- Hooks/hooks/src/components/HookUseRef.jsx
+++ Hooks/hooks/src/components/HookUseRef.jsx
@@ -8,17 +8,39 @@ const HookUseRef = () => {
 
   useEffect(() => {
     numberRef.current = numberRef.current + 1;
-    console.log('renderizou')
-  })
+    console.log("renderizou");
+  });
+
+  //2 useRef e dom
+  const inputRef = useRef();
+  const [text, setText] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setText("")
+    inputRef.current.focus();
+  }
 
   return (
     <div>
+      {/*1  useRef */}
       <h2>useRef</h2>
       <p>O componente renderizou: {numberRef.current}</p>
       <p>Counter 1: {counter}</p>
       <button onClick={() => setCounter(counter + 1)}>Contador A</button>
       <p>Counter 2: {counterB}</p>
       <button onClick={() => setCounterB(counterB + 1)}>Contador B</button>
+      <br />
+      {/* 2 - useRef e dom */}
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          ref={inputRef}
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+        />
+        <input type="submit" value="enviar" />
+      </form>
     </div>
   );
 };
